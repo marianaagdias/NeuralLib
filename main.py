73ee5f84de102e4e0821c1724481a9f60afbfe50
marianaagdias <marianaagdias97@gmail.com>
2025-04-29 15:33:13 +0100

    update gitignore

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from architectures import BaseModel, GRUseq2seq, TransformerSeq2Seq
+from NeuralLib import GRUseq2seq
 
 # Example usage
-model = GRUseq2seq(n_features=1, hid_dim=64, n_layers=3, dropout=0.3, learning_rate=0.001, results_directory='')
+model = GRUseq2seq(model_name='model1', n_features=1, hid_dim=64, n_layers=3, dropout=0.3, learning_rate=0.001)
 
 if __name__ == '__main__':
     print(model)
